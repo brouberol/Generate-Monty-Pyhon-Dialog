02543a64c946a2f7598ea17e296dc2a3a17cbb9d
Balthazar Rouberol <rouberol.b@gmail.com>
2011-11-01 12:18:21 -0200

    All in
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

---
+++ README.md
@@ -0,0 +1,51 @@
+<h1>Monty Python Text Generator</h1>
+
+<p>This project was part of my Data Compression class. The main idea is to use different Markov chains to analyse a data text and symbol probabilities to hence be able to generate a similar (but quite different!) text.</p>
+
+<h2>What does it do?</h2>
+<p>The first part consists in analysing the "Monty Python and the Holy Grail" script using Markov chains of order 10. (<a href="http://en.wikipedia.org/wiki/Markov_chain">more details on Markov chains</a>). Briefly, Markov chains are a mathematical object stating that the probability of an event depends on the k last events, whiwh is perfectly suitable for text modelling (and I believe it was the first goal of Markov). Indeed, the probability of the character 'e' is quite strong if the past is 'th'. The whole idea is to know "how long" should be this past.</p>
+
+<p>The scripts <code>py/probabilities.py</code> and <code>py/probabilities_multi.py</code> will analyse the text (for each Markov order in the list k_list) and build matrices of empirical transition probability between a k-uple (the past) and a symbol</p>
+
+<p>These two scripts perform EXACTLY the same operations and will give exact same results. However, <code>py/probabilities_multi.py</code> use the multiprocessing python library to decrease execution time. If you have a multi-core architecture, that's the script you want to use.</p>
+
+<p>The script <code>py/random_texts.py</code> will use these matrices to generate symbols based on the previous ones.</p>
+
+<h2>How to use them? </h2>
+
+<pre>
+$ python probabilities.py ../data.data.txt
+</pre>
+
+<pre>
+$ python probabilities_multi.py ../data.data.txt
+</pre>
+
+<pre>
+$ python random_text.py ../data/data.txt [output_size]
+</pre>
+
+<h2>Example</h2>
+<p>For k = 5:</p>
+
+<p>KING ARTHUR: Yes!<br/>
+VILLAGER #3: A bit.<br/>
+VILLAGER #1: You saw saw saw it, did you could<br/>
+separate, and master that!<br/>
+ARTHUR: Will you on Thursday.<br/>
+CUSTOMER: What do you can you think kill your every<br/>
+good people. It’s one.)<br/>
+OTHER FRENCH GUARDS: [whisperin</p>
+
+<p>for k = 10:</p>
+
+<p>KING ARTHUR: Will you ask your master that Arthur from the behind you looked–<br/>
+DENNIS: Oh, what a give-away. Did you hear that, eh?<br/>
+By exploiting the workers! By ’anging on to outdated imperialist dogma which perpetuates the economic and social differences</p>
+
+<p>When the context is large enough (k=10), the sentences begin to make sense (limited by the fact of the randomness, and by the fact that THIS IS MONTHY FREAKING PYTHON!)</p>
+
+<h2>Why is the first script so slow?</h2>
+<p>~95% of exec time is spent in the built-in str.count() method (cf entropia.prof file). This method being written in C and optimized, optimization techniques like using Cython/Shedskin do not apply.</p>
+
+<p>However, a parallel/multiprocessing technique can apply very easily (script <code>py/probabilities_multi.py</p>). It lead to a nice x2.8 exec speed result on my QuadCore laptop.</p> 
---
+++ py/probabilities.py
@@ -0,0 +1,156 @@
+"""
+This project analyses a data text in terms of empirical 
+probability of transition between all encountered strings
+of fixed length k and symbols belonging to the text alphabet
+to produce a matrix of conditional probabilities.
+
+This matrix can then be used in the script random_text.py to
+generate a text respecting the same probabilities, and hence
+the same "style"
+
+Author: Balthazar Rouberol @BaltoRouberol
+License : DWTFYWWI (Do What The Fuck You Want With It) 
+"""
+
+import math
+import pickle
+import sys
+from os.path import exists
+
+def alphabet(datafile_lines):
+    """
+    Returns all used characters in a given text
+    """
+
+    alph = []
+    for line in datafile_lines:
+        for letter in line:
+            if letter not in alph:
+                alph.append(letter)
+
+    return sorted(alph)
+
+def empirical_entropy_rate(datafile_lines, k, alpha, debug=False):
+    """
+    Given a datafile lines and a order k, returns the
+    empirical entropy rate for a Markov chain of order k
+    """
+
+    if debug:
+        print datafile_lines
+
+    res = 0
+
+    # -- split text in ak chunks of length k
+    ak_chunks = [datafile_lines[i:i+k] for i in xrange(len(datafile_lines))] 
+    if debug:
+        print ak_chunks
+
+    # -- remove final chunk if not of size k
+    if len(ak_chunks[-1]) != k:
+        ak_chunks.remove(ak_chunks[-1]) 
+        if debug:
+            print ak_chunks
+
+    # -- Extract unique values from list
+    ak_chunks = list(set(ak_chunks)) #set: reduce to unique values
+    if debug:
+        print ak_chunks
+    
+    # Initialization of matrix
+    prob = {}       
+    
+    for ak in ak_chunks: 
+        # New matrix line
+        prob[ak] = {}
+
+        # number of times ak encountered in text
+        nak = n_ak(datafile_lines, ak)
+
+        # -- calculate *cumulative* p(b|a^k)  for each symbol of the alphabet
+        pbak_cumul = 0
+        for char in alpha:
+            pbak = conditional_empirical_proba(datafile_lines, k, ak, char, nak)
+            pbak_cumul += pbak
+
+            # if sucession ak+char is encountered in text, add probability to matrix
+            if pbak != 0.0: # Very important, if pbak = 0.0, the combination ak+char will not be randomly generated 
+                prob[ak][char] = pbak_cumul
+
+    # Results storage
+    proba_file = open('../results/distribs/distrib_k%d.txt'%(k), 'w')
+    pickle.dump(prob, proba_file)
+    proba_file.close()
+        
+    return 0
+    
+
+def conditional_empirical_proba(chain, k, ak, symbol, n_ak): # p(b|a^k)
+    """
+    Returns the proportion of symbols after the ak string (contained
+    in chain string and of length k) which are equal to the value 
+    of given parameter 'symbol'
+    Ex : conditional_empirical_proba('ABCABD', 2, 'AB', 'C', n_ak) --> 0.5 
+    """
+
+    nb_ak = n_b_ak(chain, k, ak, symbol)
+    if n_ak != 0:
+        return float(nb_ak)/n_ak
+    else:
+        return 0
+ 
+
+def n_ak(chain, ak): # n_(a^k)
+    """
+    Given a string chain and a strink ak,
+    returns the number of times that ak
+    is found in chain
+    """
+
+    return chain.count(ak)
+
+def n_b_ak(chain, k, ak, symbol): # n_(b|a^k)
+    """
+    Given a string chain, returns the number of 
+    times that a given symbol is found 
+    right after a string ak inside the chain one
+    """
+ 
+    if k != 0:
+        res = chain.count(ak+symbol)
+    else:
+        # if k=0, p(b|a^k) = p(b), because the past has no influence
+        res = chain.count(symbol)
+    return res
+    
+
+if __name__=='__main__':
+
+    EXIT_MSG = 'The program will now end.'
+    
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+
+        if exists(filepath):
+            f = open(filepath,'r')
+            f_lines = ' '.join(f.readlines()) # all text in a big string
+            f.close()
+            
+            # deduce alphabet from text
+            alph = alphabet(f_lines)
+ 
+            k_list = [10]
+            for k in k_list: 
+                print 'Generating probability matrix for text %s '%(filepath) 
+                # Put debug = True to enable print operations
+                # Use only with small data, otherwise, useless >_<
+                empirical_entropy_rate(f_lines, k, alph, debug=False)
+        
+        else:
+            print 'The given file path %s leads to no valid file.' %(filepath)
+            print EXIT_MSG
+            exit(2)
+    else:
+        print 'No file path given in argument.'
+        print EXIT_MSG
+        exit(2)
